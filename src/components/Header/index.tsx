8fe38b10b334882df9bdb44179716b31fde9380b
Felipe Scherer <felipescherer25@gmail.com>
2022-06-06 23:30:42 -0300

    feat: theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
 const Header = () => {
+
+
+  function changeTheme() {
+    let theme = document.documentElement.getAttribute('data-theme');
+    document.documentElement.setAttribute('data-theme', theme === 'light' ? 'dark' : 'light');
+  }
+
   return (
-    <div className="p05 flex flex-column">
+    <div className="p05 flex flex-column bg-primary">
       <h1 className="font-size-title">What Happened in that Day</h1>
 
       <span className="font-size-text mt05">Code: mockado</span>
+      <button onClick={changeTheme}>Troca</button>
     </div>
   )
 }
