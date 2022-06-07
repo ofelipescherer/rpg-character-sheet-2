7a20c6fb2545c225017f839e12e84b985da16ee5
Felipe Scherer <felipescherer25@gmail.com>
2022-06-06 21:58:10 -0300

    feat: first component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+const Header = () => { 
+  return (
+    <div className="p05 flex flex-column">
+      <h1 className="font-size-title">What Happened in that Day</h1> 
+
+      <span className="font-size-text mt05">Code: mockado</span>
+    </div>
+  )
+}
+
+export default Header
