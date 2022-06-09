25475e7814f44a1dc7c9af031b3fb2cbbda14963
Felipe Scherer <felipescherer25@gmail.com>
2022-06-08 22:50:36 -0300

    feat: theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,12 @@ const Header = () => {
     let theme = document.documentElement.getAttribute('data-theme')
     document.documentElement.setAttribute(
       'data-theme',
-      theme === 'light' ? 'dark' : 'light'
+      theme === 'dark' ? 'light' : 'dark'
     )
   }
 
   return (
-    <div className="p05 flex flex-column bg-primary">
+    <div className="p05 flex flex-column accent-container">
       <h1 className="font-size-title">What Happened in that Day</h1>
 
       <span className="font-size-text mt05">Code: mockado</span>
